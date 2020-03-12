70374a61857c9560711da5e7b229b6c4803693a2
t-pou <t-pou@DESKTOP-39H26U1>
2020-03-12 10:18:11 -0600

    configuring project locations

---
+++ Practice/DuplicateChecker.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class DuplicateChecker {
+
+	int[] array;
+	ArrayList duplicates;
+	int duplicateCount, duplicateIndex;
+	
+	public DuplicateChecker(int[] array1) {
+		array = array1;
+		duplicateCount = duplicateIndex = 0;
+		duplicates = new ArrayList();
+		
+		for (int i = 0; i < array.length-1; i++) {
+			for (int u = i+1; u < array.length; u++) {
+				if (array[i] == array[u]) {
+					duplicates.add(array[i]);
+					duplicateCount++;
+				}	
+			}
+		}	
+	}
+	
+	public int getDuplicateCount() {
+		return duplicateCount;
+	}
+	
+	public String getDuplicates() {
+		String result = "";
+		for (int i = 0; i < duplicates.size(); i++) {
+			result += duplicates.get(i) + "\n";
+		}
+		return result;
+	}	
+}
