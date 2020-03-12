6889460d9f4237fe25e84aa13214613c75d803e2
tpoulsen <t-poulsen@hotmail.com>
2020-03-12 10:49:10 -0600

    more git practice

--- Practice/DuplicateChecker.java
+++ Practice/DuplicateChecker.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 public class DuplicateChecker {
 
 	int[] array;
-	ArrayList duplicates;
+	ArrayList<Integer> duplicates;
 	int duplicateCount, duplicateIndex;
 	
 	public DuplicateChecker(int[] array1) {
 		array = array1;
 		duplicateCount = duplicateIndex = 0;
-		duplicates = new ArrayList();
+		duplicates = new ArrayList<Integer>();
 		
 		for (int i = 0; i < array.length-1; i++) {
 			for (int u = i+1; u < array.length; u++) {
---
+++ Practice/TestSuite.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestSuite {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
